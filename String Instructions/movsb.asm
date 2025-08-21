9ab9e35338e9f4383a02f583ddc4313b5369d9f2
Sumit Desai <sumit.desai3433@gmail.com>
2025-08-21 11:43:04 +0530

    Added MODSW Instruction

--- String Instructions/movsb.asm
+++ String Instructions/movsb.asm
@@ -4,12 +4,10 @@ MOV DS, AX
 MOV ES, AX
 MOV SI, 2000H
 MOV DI, 2010H
-CLD ; DF(direction flag) = 0 ; SI will be incremeneted by
-; 1 
+CLD ; DF(direction flag) = 0 ; SI will be incremeneted by 1
 MOV [2000H], 10H
 MOV [2001H], 20H
-MOVSB ; copies contents of bytes (automatically increments if DF = 0)
-; decrements if DF = 1
+MOVSB ; copies contents of bytes (automatically increments if DF = 0) decrements if DF = 1
 MOVSB
 HLT
 ret
---
+++ String Instructions/movsw.asm
@@ -0,0 +1,17 @@
+org 100h
+
+MOV AX, 2000H
+; CLD (CLEAR DIRECTION FLAG)
+MOV DS, AX
+MOV ES, AX
+MOV SI, 2000H
+MOV DI, 2010H
+CLD ; here DF is set to zero  
+MOV [2000H], 40H
+MOV [2001H], 50H
+MOVSW ; move string word   
+;  move word from DS:SI -> ES:DI (2 bytes)
+HLT
+
+ret
+; SB - String bytes (incremented/decremented by 1), SW - String words (incremented/decremented by 2)
