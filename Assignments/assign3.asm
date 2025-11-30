45ce7e71134a961b6b2e9b79e0da25658ff22cfa
Sumit Desai <sumit.desai3433@gmail.com>
2025-11-30 13:22:08 +0530

    Added Assembly Codes

--- Assignments/assign3.asm
+++ Assignments/assign3.asm
@@ -1,4 +1,4 @@
-; Q1.
+; Q1.Write an ALP to perform 8-bit multiplication
 org 100h
 MOV AL, 02H
 MOV BL, 04H
@@ -6,7 +6,7 @@ MUL BL
 HLT
 ret          
                     
-; Q.2
+; Q.2 Write an ALP to perform 16-bit multiplication
 
 org 100h
 MOV AX, 2222H
@@ -15,29 +15,31 @@ MUL BX
 HLT
 ret
 
-;Q.3
+;Q.3 Write an ALP to perform Division of 16 bit number by 8 bit number
 org 100h
 
 MOV AX, 0064H       
 MOV BL, 05H         
 DIV BL              
+; Quotient is in AL & Remainder is in AH
 
 HLT
 ret
 
-; Q.4
+; Q.4 Write an ALP to perform Division of 32 bit number by 16 bit number
 org 100h
 
 MOV DX, 0001H       
 MOV AX, 0000H         
                     
 MOV CX, 0008H       
-DIV CX             
+DIV CX   
+; Quotient is in AX & Remainder is in DX
 
 HLT    
 ret
 
-;Q.5
+;Q.5 Finding the Fibonacci series of n terms
 .model small
 .stack 100h
 .code
@@ -45,18 +47,18 @@ main proc
     MOV AH, 01H
     INT 21H
     SUB AL, '0'
-    MOV CL, AL
-    MOV AX, 0000H
-    MOV BX, 0001H
+    MOV CL, AL ; counter
+    MOV AX, 0000H ; prev
+    MOV BX, 0001H ; curr
 fib_loop:
     ADD AX, BX
-    XCHG AX, BX
+    XCHG AX, BX 
     LOOP fib_loop
     HLT
 main endp
 end main
 
-;Q.6 a
+;Q.6 a Perform - (a + b) * (c + d)
 MOV AX, 05H
 MOV BX, 03H
 MOV CX, 04H
@@ -68,7 +70,7 @@ MUL CX
 
 HLT
 
-;Q.6.b
+;Q.6.b Perform - (a * b) + (c * d)
 MOV AX, 05H
 MOV BX, 03H
 MOV CX, 04H
@@ -83,7 +85,7 @@ ADD AX, BX
 HLT
 
 
-;Q.7
+;Q.7 Matrix addition and subtraction
 MOV SI, 2000H
 MOV DI, 2010H
 MOV BX, 2020H
@@ -99,7 +101,7 @@ addition:
 LOOP addition
 HLT
 
-;Q.8
+;Q.8 Find Factorial of a Number Using Recursion
 .model small
 .stack 100h
 .code
